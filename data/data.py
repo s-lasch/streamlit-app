faece7fde578bc9ea5a4e30063bc0c07495cfab6
Steven Lasch <stevenlasch17@gmail.com>
2024-02-14 13:45:30 -0500

    Change files.

--- data/data.py
+++ data/data.py
@@ -136,7 +136,7 @@ def get_mode_data(df, col=None, lang='All'):
                             df_box[col][df_box['mode'] == 'custom'].to_list(),
                             df_box[col][df_box['mode'] == 'zen'].to_list()]]
 
-        boxplot['dataset'][0] = data
+        boxplot['dataset'][0]['source'] = data
         # boxplot['dataset'][1]['transform']['config']['itemNameFormatter'] = 'expr {value}'
 
     return pie, boxplot
